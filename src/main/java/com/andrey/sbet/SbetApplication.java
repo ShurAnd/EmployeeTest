8b92a69f49ea3b8d2e40b74abf888deca803f4c6
AASh <AASh@mail.com>
2020-03-15 00:07:13 +0300

    initial commit

---
+++ src/main/java/com/andrey/sbet/SbetApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.sbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey/sbet/SbetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.sbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
